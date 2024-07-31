5ecdd79b8e7c2b7a39305b11cf6d53df2cf7d822
Billy Hasman <billyhasman08@gmail.com>
2024-07-31 17:16:58 +0700

    Add_Style on Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -8,19 +8,11 @@ export default function Navbar() {
   }
 
   return (
-    <nav className='bg-white border-gray-200 dark:bg-gray-900'>
+    <nav className='bg-gray-800 border-gray-200'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <a
-          href='https://flowbite.com/'
-          className='flex items-center space-x-3 rtl:space-x-reverse'
-        >
-          <img
-            src='https://flowbite.com/docs/images/logo.svg'
-            className='h-8'
-            alt='Flowbite Logo'
-          />
+        <a href='#' className='flex items-center space-x-3 rtl:space-x-reverse'>
           <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-            Flowbite
+            Cashier App
           </span>
         </a>
         <div className='flex md:order-2'>
@@ -70,7 +62,7 @@ export default function Navbar() {
             <input
               type='text'
               id='search-navbar'
-              className='block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='block w-full p-2 ps-10 text-sm text-gray-400 border border-gray-300 rounded-lg bg-gray-50 focus:ring-white-500 focus:border-white-500 '
               placeholder='Search...'
             />
           </div>
@@ -124,15 +116,15 @@ export default function Navbar() {
             <input
               type='text'
               id='search-navbar'
-              className='block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='block w-full p-2 ps-10 text-sm text-gray-400 border border-gray-300 rounded-lg bg-gray-50 focus:ring-white-500 focus:border-white-500 '
               placeholder='Search...'
             />
           </div>
-          <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+          <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 '>
             <li>
               <a
                 href='#'
-                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                className='block py-2 px-3 focus:text-white text-gray-400 bg-white-700 rounded md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white-500'
                 aria-current='page'
               >
                 Home
@@ -142,7 +134,7 @@ export default function Navbar() {
               <button
                 id='dropdownNavbarLink'
                 data-dropdown-toggle='dropdownNavbar'
-                className='flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent'
+                className='flex items-center justify-between w-full py-2 px-3 text-gray-400 focus:text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 md:w-auto'
                 onClick={toggleDropdown}
               >
                 Dropdown{' '}
@@ -166,16 +158,16 @@ export default function Navbar() {
               {dropdownOpen && (
                 <div
                   id='dropdownNavbar'
-                  className='absolute z-10 mt-2 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600'
+                  className='absolute z-10 mt-2 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 '
                 >
                   <ul
-                    className='py-2 text-sm text-gray-700 dark:text-gray-400'
+                    className='py-2 text-sm text-gray-700 '
                     aria-labelledby='dropdownLargeButton'
                   >
                     <li>
                       <a
                         href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
+                        className='block px-4 py-2 hover:bg-gray-100 '
                       >
                         Dashboard
                       </a>
@@ -183,7 +175,7 @@ export default function Navbar() {
                     <li>
                       <a
                         href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
+                        className='block px-4 py-2 hover:bg-gray-100 '
                       >
                         Settings
                       </a>
@@ -191,7 +183,7 @@ export default function Navbar() {
                     <li>
                       <a
                         href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
+                        className='block px-4 py-2 hover:bg-gray-100 '
                       >
                         Earnings
                       </a>
@@ -200,7 +192,7 @@ export default function Navbar() {
                   <div className='py-1'>
                     <a
                       href='#'
-                      className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
+                      className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 '
                     >
                       Sign out
                     </a>
@@ -211,7 +203,7 @@ export default function Navbar() {
             <li>
               <a
                 href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                className='block py-2 px-3 text-gray-400 focus:text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white-700 md:p-0 '
               >
                 About
               </a>
@@ -219,7 +211,7 @@ export default function Navbar() {
             <li>
               <a
                 href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                className='block py-2 px-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white-700 md:p-0'
               >
                 Services
               </a>
