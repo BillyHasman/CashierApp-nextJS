ec6c78a412d7b744ba55702cd2acddc520525d13
Billy Hasman <billyhasman08@gmail.com>
2024-08-01 12:06:22 +0700

    Add_Components and ComponentsIndex

---
+++ src/app/components/Hasil.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+export default function Hasil() {
+  return (
+    <div className='text-black text-2xl font-bold w-1/3'>
+      <p>Hasil</p>
+      <hr className='mt-3'></hr>
+    </div>
+  )
+}
---
+++ src/app/components/ListCategories.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+export default function ListCategories() {
+  return (
+    <div className='w-1/3 text-black text-2xl font-bold'>
+      <p>Daftar Kategori</p>
+      <hr className='mt-3'></hr>
+    </div>
+  )
+}
