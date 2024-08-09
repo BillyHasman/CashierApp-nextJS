90f25d64b4042b9512d7d6cc0cf3010763c94a1b
Billy Hasman <billyhasman08@gmail.com>
2024-08-09 20:08:04 +0700

    Add_formatCurrency Function to Global

---
+++ src/app/utils/helpers.js
@@ -0,0 +1,9 @@
+// Format Rupiah
+export function formatCurrency(value) {
+  return value.toLocaleString('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
