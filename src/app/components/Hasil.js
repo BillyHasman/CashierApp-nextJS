7642e9a9b605ac584b12e4cd3fee7741d0fbb861
Billy Hasman <billyhasman08@gmail.com>
2024-08-12 14:09:28 +0700

    Fix_Container margin

--- src/app/components/Hasil.js
+++ src/app/components/Hasil.js
@@ -1,6 +1,7 @@
 import { React, Component } from 'react'
 
 import { formatCurrency } from '../utils/helpers'
+import TotalBayar from './TotalBayar'
 
 export default function Hasil({ keranjangs }) {
   console.log('item: ', keranjangs)
@@ -14,18 +15,18 @@ export default function Hasil({ keranjangs }) {
           <div>
             {keranjangs.map((menuKeranjang) => (
               <div className='flex w-full px-4 py-2 border-b border-gray-200 rounded hover:bg-gray-100 hover:text-gray-700 focus:text-white focus:bg-gray-700'>
-                <div className='mx-auto my-auto'>
+                <div className=' mx-auto my-auto'>
                   <div className='badge badge-success badge-lg text-lg text-white'>
                     {menuKeranjang.jumlah}
                   </div>
                 </div>
-                <div className='flex flex-col w-1/2 ml-3'>
+                <div className='flex flex-col w-1/2 px-5'>
                   <div className='text-lg'>{menuKeranjang.product.nama}</div>
-                  <div className='text-md'>
+                  <div className='text-md font-normal'>
                     {formatCurrency(menuKeranjang.product.harga)}
                   </div>
                 </div>
-                <div className='flex justify-end w-1/3 font-bold mr-5'>
+                <div className='flex justify-end w-1/3 font-bold mr-5 '>
                   {formatCurrency(menuKeranjang.total_harga)}
                 </div>
               </div>
@@ -33,6 +34,7 @@ export default function Hasil({ keranjangs }) {
           </div>
         )}
       </div>
+      <TotalBayar keranjangs={keranjangs} />
     </div>
   )
 }
